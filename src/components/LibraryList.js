0c2dc82d7df1535404fae5d5bf03762e28670a33
Terry Wang <terrywang@tianyu.ninja>
2018-06-21 22:59:35 -0400

    connect

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render() {
+    
     return;
   }
 }
 
-export default connect()(LibraryList);
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
